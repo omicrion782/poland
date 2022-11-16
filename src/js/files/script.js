5c104e486b24ed903f87f800472a9f727541fcb2
omicrion782 <litvat@inbox.ru>
2022-11-16 22:34:13 +0300

    start

---
+++ src/js/files/script.js
@@ -0,0 +1,12 @@
+// Подключение функционала "Чертогов Фрилансера"
+import { isMobile } from "./functions.js";
+// Подключение списка активных модулей
+import { flsModules } from "./modules.js";
+
+// slick slider
+
+
+
+
+
+
